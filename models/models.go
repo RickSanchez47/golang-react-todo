9a791847d7b685e78eccb79de904c52f455b23eb
SREEARG KJ <sreeragkj7@gmail.com>
2024-03-18 21:09:39 +0530

    model

--- models/models.go
+++ models/models.go
@@ -1,5 +1,11 @@
 package models
 
-import(
+import (
 	"go.mongodb.org/mongo-driver/mongo/bson/primitive"
 )
+
+type TodoList struct {
+	ID     primitive.ObjectId `json:"_id,omitempty" bson:"_id,omitempty"`
+	Task   string
+	Status bool
+}
